652708448dc6ca256b11ea2fb84af33cce6c3d23
chandan <chandanchandru45@gmail.com>
2025-01-20 23:26:01 +0530

    001

---
+++ Strings/StringMethods.py
@@ -0,0 +1,11 @@
+#Different ways of Declaraing the strings :
+s1 = "I'm Chandan"
+s2 = 'My "Age" is 24'
+s3 = """We are Learning 
+         Strings"""
+s4 = '''Python is Dynamically 
+         Typed Language'''
+print(s1)
+print(s2)
+print(s3)
+print(s4)
